3c55abb5f2f65d021ef9eec35b2931c39b5671b2
Shaomonk <shaomonk93@yandex.ru>
2019-01-09 19:46:05 +0300

    допилины About формы для проги и бд

--- RK/RK/FormAboutBD.Designer.cs
+++ RK/RK/FormAboutBD.Designer.cs
@@ -28,11 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAboutBD));
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(143, 181);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(182, 34);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "Отмена";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(445, 65);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // FormAboutBD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FormAboutBD";
+            this.ClientSize = new System.Drawing.Size(479, 237);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.Name = "FormAboutBD";
+            this.Text = "О базе данных";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
     }
 }
--- RK/RK/FormAboutProg.Designer.cs
+++ RK/RK/FormAboutProg.Designer.cs
@@ -28,11 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAboutProg));
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(154, 277);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(182, 33);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Отмена";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 169);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // FormAboutProg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FormAboutProg";
+            this.ClientSize = new System.Drawing.Size(479, 333);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOk);
+            this.Name = "FormAboutProg";
+            this.Text = "О программе";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label1;
     }
 }
